1e803f820c18181538f997dd724225f9c5328222
Anolay900 <andalrom@gmail.com>
2021-07-26 22:01:25 +0300

    top10

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,12 @@ $(document).ready(function(){
         pauseOnFocus: true,
         variableWidth: true,
     });
+
+    $('.slidertop').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        slidesToShow: 5,
+    });
 });
