b9fa853049180132bd7bef76553fbfa68f3b2686
Anolay900 <andalrom@gmail.com>
2021-07-26 23:29:16 +0300

    arrivals

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,12 @@ $(document).ready(function(){
         pauseOnFocus: true,
         slidesToShow: 5,
     });
+
+    $('.sliderbook').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        slidesToShow: 2,
+    });
 });
