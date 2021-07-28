c48ceedc08c7218315ae127210214181d986b1eb
Anolay900 <andalrom@gmail.com>
2021-07-28 20:33:40 +0300

    bookpagebegining

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,19 @@ $(document).ready(function(){
         slidesToShow: 2,
     });
 });
+
+function readMore() {
+    var dots = document.getElementById("dots");
+    var more = document.getElementById("more");
+    var btn = document.getElementById("btn");
+
+    if(dots.style.display === "none") {
+        dots.style.display="inline";
+        btn.innerHTML="read more";
+        more.style.display="none";
+    } else {
+        dots.style.display="none";
+        btn.innerHTML="hide";
+        more.style.display="inline";
+    }
+}
